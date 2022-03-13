0a3f6f626025e6f7e36ecebc16430c11f4ebf81b
amriwahab01 <101490168+amriwahab01@users.noreply.github.com>
2022-03-13 13:02:28 +0800

    Add files via upload

---
+++ variable dan tipe data.py
@@ -0,0 +1,25 @@
+#operato
+a = 4
+b = 7
+#operator aritmatika
+#operator penjumlahan
+hasil = a + b
+print(hasil)
+#pengurangan
+hasil = a - b
+print(hasil)
+#pembagian
+hasil = a / b
+print(hasil)
+#perkalian
+hasil = a * b
+print(hasil)
+#pangkat
+hasil = a ** b
+print(hasil)
+#sisa bagi
+hasil = a % b
+print(hasil)
+#pembagian bulat
+hasil = a // b
+print(hasil)
